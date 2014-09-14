965aaf0529ba8f4420dd8b1cc3e29b144c479f24
k-miyake <3katsu3hiro@gmail.com>
2014-09-14 18:21:16 +0900

    chapter28_2_等価メソッドの開発終了

---
+++ src/main/scala/chpater28_ObjectEquality/ColoredPoint.scala
@@ -0,0 +1,17 @@
+package chpater28_ObjectEquality
+
+/**
+ * Created by katsuki on 2014/09/14.
+ */
+class ColoredPoint extends Point{
+
+  override def equals(other: Any) = other match {
+    case that:ColoredPoint =>
+      that canEqual this
+    case _ =>
+      false
+
+  }
+
+  override def canEqual(other: Any) = other.isInstanceOf[ColoredPoint]
+}
