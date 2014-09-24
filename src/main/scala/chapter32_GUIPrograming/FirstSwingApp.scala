0aaf94f003e95c16bb34c706e2bd696fa73af3e4
k-miyake <3katsu3hiro@gmail.com>
2014-09-25 00:20:06 +0900

    chapter32_2_パネルとレイアウト_終了

--- src/main/scala/chapter32_GUIPrograming/FirstSwingApp.scala
+++ src/main/scala/chapter32_GUIPrograming/FirstSwingApp.scala
@@ -8,8 +8,16 @@ import scala.swing._
 object FirstSwingApp extends SimpleSwingApplication{
   override def top: Frame = new MainFrame(){
     title = "First Swing App"
-    contents = new Button {
+    val button = new Button {
       text = "Click me"
+    }
+    val label = new Label {
+      text = "No button clicks registered"
+    }
+    contents = new BoxPanel(Orientation.Vertical){
+      contents += button
+      contents += label
+      border = Swing.EmptyBorder(30,30,10,30)
     }
   }
 }
